fdc51be7ee037a9c22c227802eec3c78f93c464d
gavinwang <wwwangwei159@sina.com>
2017-02-25 16:20:56 +0800

    add stack and queue method

--- javaTestPorject/src/javatest/stackheap/stack.java
+++ javaTestPorject/src/javatest/stackheap/stack.java
@@ -1,25 +1,40 @@
 package javatest.stackheap;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 
 public class stack {
 
 
+	//先进后出
 	    public void testHeap(){
-	        for(;;){
-	              ArrayList list = new ArrayList (2000);
-	          }
+	        LinkedList<Integer> stack = new LinkedList();
+	        stack.push(1);
+	        stack.push(2);
+	        stack.push(7);
+	        stack.push(9);
+	        for(Iterator it = stack.iterator();it.hasNext();){
+	        	System.out.println("i==="+stack.pop());
+	        }
 	    }
-	    int num=1;
+	    //先进先出
 	    public void testStack(){
-	        num++;
-	        this.testStack();
+	    	 LinkedList<Integer> queue = new LinkedList();
+	    	 queue.offer(3);
+	    	 queue.offer(1);
+	    	 queue.offer(4);
+	    	 queue.offer(5);
+	    	 for(Iterator it = queue.iterator();it.hasNext();){
+		        	System.out.println("i==="+queue.poll());
+		        }
 	     }
 	    
 	    public static void main(String[] args){
 	    	stack  t  = new stack ();
 	        t.testHeap();
-	        //t.testStack();   
+	        t.testStack();   
 	    }
 	
 
